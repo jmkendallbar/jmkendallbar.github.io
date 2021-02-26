62409faab67c899566331bbdc2427fbd88de0c56
Jessica Kendall-Bar <jkb@ucsc.edu>
2021-02-25 17:22:06 -0800

    adding data in csv format
    also fixed d3 source reference by adding it to the body section instead of the header

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,10 +2,10 @@ console.log("Hello from Javascript")
 // alert("Welcome to Art for Science Communication!")
 
 let portfolio = [
-    {artist_src: "https://news.ucsc.edu/2019/07/images/kendall-bar-jessica.jpg", artist: "Jessica Kendall-Bar", project: "Elephant Seal Animation"},
-    {artist_src: "https://news.ucsc.edu/2019/07/images/kendall-bar-jessica.jpg", artist: "Jessica Kendall-Bar", project: "Elephant Seal Animation"},
-    {artist_src: "https://news.ucsc.edu/2019/07/images/kendall-bar-jessica.jpg", artist: "Jessica Kendall-Bar", project: "Elephant Seal Animation"},
-    {artist_src: "https://news.ucsc.edu/2019/07/images/kendall-bar-jessica.jpg", artist: "Jessica Kendall-Bar", project: "Elephant Seal Animation"},
+    {artist_src: "https://avatars0.githubusercontent.com/u/57967704?v=4&s=128", artist: "Jessica Kendall-Bar", project: "Elephant Seal Animation"},
+    {artist_src: "https://avatars0.githubusercontent.com/u/57967704?v=4&s=128", artist: "Jessica Kendall-Bar", project: "Elephant Seal Animation"},
+    {artist_src: "https://avatars0.githubusercontent.com/u/57967704?v=4&s=128", artist: "Jessica Kendall-Bar", project: "Elephant Seal Animation"},
+    {artist_src: "https://avatars0.githubusercontent.com/u/57967704?v=4&s=128", artist: "Jessica Kendall-Bar", project: "Elephant Seal Animation"},
   ]
 console.log(portfolio)
 
@@ -17,3 +17,14 @@ let sel = d3.select("#gallery")
 sel.append("p").text((d) => d.artist)  /* d.species ? d.species : "don't know" */
 sel.append("img").attr("src", d => d.artist_src).attr("title", d => d.artist)
 .style("max-width", "400px")
+
+// Now let's try with a csv
+
+let csv_data = d3.csv("/data/artists_data.csv", function(data) {
+    for (var i = 0; i < data.length; i++) {
+        console.log(data[i].artist_src);
+        console.log(data[i].project);
+    }
+});
+
+console.log(csv_data)
