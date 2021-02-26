da40633acf626bd8239d8982fdcbb3192d586af3
Jessica Kendall-Bar <57967704+jmkendallbar@users.noreply.github.com>
2021-02-26 00:30:37 -0800

    Added examples with loading csv and using fetch functions

--- javascripts/main.js
+++ javascripts/main.js
@@ -32,3 +32,112 @@ csv_data.then(function(result){
 });
 
 console.log(csv_data)
+
+// Fetch example with local file using .then method
+// console.log("about to fetch kelp picture saved as algae.jpg in data folder")
+// fetch("/data/algae.jpg").then(response => { // Call fetch function with a local path to image 
+//     console.log(response);
+//     return response.blob(); // then we get a response
+// }).then(kelp_blob => {
+//     console.log(kelp_blob); // grab the data in the response
+//     document.getElementById('kelp').src = URL.createObjectURL(kelp_blob); // update img element with your data
+// }).catch(error => { //Logging errors
+//     console.log("Error! See below.") //Printing text to show error
+//     console.error(error); //Showing error message
+// });
+
+// Fetch example with local file using async and await instead of code above
+async function catchKelp() {
+    const response = await fetch("/data/algae.jpg");
+    const kelp_blob = await response.blob();
+    document.getElementById('kelp').src = URL.createObjectURL(kelp_blob); // update img element with your data
+}
+
+// Calling the asynchronous function to display the fetched picture
+catchKelp()
+    .then(response => {
+        console.log('yay kelp')
+    })
+    .catch(error => { //Logging errors
+    console.log("Error with kelp image! See below.") //Printing text to show error
+    console.error(error); //Showing error message
+});
+
+// Now fetching Elephant Seal Census data from a CSV
+// Data cleaned from https://datadryad.org/stash/dataset/doi:10.7291/D1PP47
+
+async function getData() {
+    const response = await fetch("/data/Elephant Seal Census Data_smaller.csv");
+    const census_data = await response.text();
+    // console.log(census_data); // Preview data
+    // Now parsing the data with separators
+    const table = census_data.split('\n').slice(1); 
+    // Using \n to show us where each new line is and taking out first row
+    // then write .forEach loop
+    table.forEach(row => {
+        const columns = row.split(',');
+        const Census_ID = columns[0]; // Naming columns
+        const Observer = columns[1]; 
+        const agesexclass = columns[9];
+        const population = columns[11];
+        xlabels.push(Census_ID);
+        pop_estimate.push(population);
+        console.log(Census_ID,Observer);
+    });
+};
+
+getData()
+    .then(response => {
+    console.log('yay data') // Celebrating successful data input
+}).catch(error => { // Catching errors
+    console.log("Error with data input! See below.") // Printing message to show error
+    console.error(error); // Showing error message
+});
+
+const xlabels = [];
+const pop_estimate = [];
+
+MakeChart();
+
+// Charting example with Chart.js
+async function MakeChart() {
+    await getData()
+    const ctx = document.getElementById('chart').getContext('2d');
+    const myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: xlabels,
+            datasets: [{
+                label: 'Population Estimate',
+                data: pop_estimate,
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+    });
+}
+
