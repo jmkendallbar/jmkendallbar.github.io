529cbc4b86d15f2b889dd6ac26ff4492bed2d09d
Jessica Kendall-Bar <jkb@ucsc.edu>
2021-02-26 19:15:00 -0800

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -82,7 +82,7 @@ async function getData() {
         const population = columns[11];
         xlabels.push(Census_ID);
         pop_estimate.push(population);
-        console.log(Census_ID,Observer);
+        // console.log(Census_ID,Observer);
     });
 };
 
@@ -97,47 +97,47 @@ getData()
 const xlabels = [];
 const pop_estimate = [];
 
-MakeChart();
+// MakeChart();
 
-// Charting example with Chart.js
-async function MakeChart() {
-    await getData()
-    const ctx = document.getElementById('chart').getContext('2d');
-    const myChart = new Chart(ctx, {
-        type: 'bar',
-        data: {
-            labels: xlabels,
-            datasets: [{
-                label: 'Population Estimate',
-                data: pop_estimate,
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
-                ],
-                borderColor: [
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
-                ],
-                borderWidth: 1
-            }]
-        },
-        options: {
-            scales: {
-                yAxes: [{
-                    ticks: {
-                        beginAtZero: true
-                    }
-                }]
-            }
-        }
-    });
-}
+// // Charting example with Chart.js
+// async function MakeChart() {
+//     await getData()
+//     const ctx = document.getElementById('chart').getContext('2d');
+//     const myChart = new Chart(ctx, {
+//         type: 'bar',
+//         data: {
+//             labels: xlabels,
+//             datasets: [{
+//                 label: 'Population Estimate',
+//                 data: pop_estimate,
+//                 backgroundColor: [
+//                     'rgba(255, 99, 132, 0.2)',
+//                     'rgba(54, 162, 235, 0.2)',
+//                     'rgba(255, 206, 86, 0.2)',
+//                     'rgba(75, 192, 192, 0.2)',
+//                     'rgba(153, 102, 255, 0.2)',
+//                     'rgba(255, 159, 64, 0.2)'
+//                 ],
+//                 borderColor: [
+//                     'rgba(255, 99, 132, 1)',
+//                     'rgba(54, 162, 235, 1)',
+//                     'rgba(255, 206, 86, 1)',
+//                     'rgba(75, 192, 192, 1)',
+//                     'rgba(153, 102, 255, 1)',
+//                     'rgba(255, 159, 64, 1)'
+//                 ],
+//                 borderWidth: 1
+//             }]
+//         },
+//         options: {
+//             scales: {
+//                 yAxes: [{
+//                     ticks: {
+//                         beginAtZero: true
+//                     }
+//                 }]
+//             }
+//         }
+//     });
+// }
 
